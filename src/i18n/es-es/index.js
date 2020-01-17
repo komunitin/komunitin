9921086d0db43176fb4894e4b835244bf709ba36
edumag <edu@lesolivex.com>
2020-01-17 18:45:18 +0100

    Add translations

--- src/i18n/es-es/index.js
+++ src/i18n/es-es/index.js
@@ -3,15 +3,20 @@
 
 export default {
   home: "Inico",
+  "Log in": "Log in",
+  'Create a new exchange': "Crear una nueva comunidad",
+  menu: "Menú",
   welcome_to: "Benvingut/da a",
   failed: "Error en proceso",
   success: "Proceso concluido",
   komuniti_is: "Komunitin es una plataforma de codi obert que ofereix la gestió de moneda social per a les comunitats d'intercanvi.",
   new_in_komunitin: "Nou/ nova a komunitin?",
+  use_location: "Use location",
   find_exchange: "Troba una xarxa d'intercanvi prop de tu i comença a utilitzar moneda social",
   or_select: "o selecciona un país per trobar-la.",
   select_country: "Select a country",
   find_comunnity: "Troba la teva comunitat i comença a participar.",
   if_not_have_community: "Si no tens una comunitat a prop teu sempre pots crear-la i començar una nova",
+  new_exchange: "New Exchange",
   need_help: "Necessites ajuda?"
 };
