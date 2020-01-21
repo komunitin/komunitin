d6e506123ec4b4a157d887e45cc92519f17fd541
edumag <edu@lesolivex.com>
2020-01-21 19:10:03 +0100

    Corrections in translations

--- src/i18n/en-us/index.js
+++ src/i18n/en-us/index.js
@@ -3,20 +3,24 @@
 
 export default {
   home: "Home",
+  Login: "Login",
   "Log in": "Log in",
-  menu: "Menu",
-  'Create a new exchange': "Create a new exchange",
+  Menu: "Menu",
+  "Create a new exchange": "Create a new exchange",
   welcome_to: "Welcome to",
   failed: "Action failed",
   success: "Action was successful",
-  komuniti_is: "Komunitin is an open source platform that offers social currency management for exchange communities.",
+  komuniti_is:
+    "Komunitin is an open source platform that offers social currency management for exchange communities.",
   new_in_komunitin: "New to komunitin?",
-  find_exchange: "Find an exchange network near you and start using social currency",
+  find_exchange:
+    "Find an exchange network near you and start using social currency",
   use_location: "Use location",
   or_select: "or select a country to find it",
   select_country: "Select a country",
   find_comunnity: "Find your community and start participating.",
-  if_not_have_community: "If you don't have a community near you, you can always create one and start a new one",
+  if_not_have_community:
+    "If you don't have a community near you, you can always create one and start a new one",
   new_exchange: "New Exchange",
   help: "Help",
   need_help: "You need help?",
--- src/i18n/es-es/index.js
+++ src/i18n/es-es/index.js
@@ -3,20 +3,24 @@
 
 export default {
   home: "Inico",
-  "Log in": "Log in",
-  'Create a new exchange': "Crear una nueva comunidad",
-  menu: "Menú",
+  Login: "Log in",
+  "Log in": "Inicia sesión",
+  "Create a new exchange": "Crear una nueva comunidad",
+  Menu: "Menú",
   welcome_to: "Benvingut/da a",
   failed: "Error en proceso",
   success: "Proceso concluido",
-  komuniti_is: "Komunitin es una plataforma de codi obert que ofereix la gestió de moneda social per a les comunitats d'intercanvi.",
+  komuniti_is:
+    "Komunitin es una plataforma de codi obert que ofereix la gestió de moneda social per a les comunitats d'intercanvi.",
   new_in_komunitin: "Nou/ nova a komunitin?",
   use_location: "Use location",
-  find_exchange: "Troba una xarxa d'intercanvi prop de tu i comença a utilitzar moneda social",
+  find_exchange:
+    "Troba una xarxa d'intercanvi prop de tu i comença a utilitzar moneda social",
   or_select: "o selecciona un país per trobar-la.",
   select_country: "Select a country",
   find_comunnity: "Troba la teva comunitat i comença a participar.",
-  if_not_have_community: "Si no tens una comunitat a prop teu sempre pots crear-la i començar una nova",
+  if_not_have_community:
+    "Si no tens una comunitat a prop teu sempre pots crear-la i començar una nova",
   new_exchange: "New Exchange",
   need_help: "Necessites ajuda?"
 };
