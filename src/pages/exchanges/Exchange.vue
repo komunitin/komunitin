231152cf7b7def1e74ab1d8afba9beb9da926f73
edumag <edu@lesolivex.com>
2020-02-17 12:27:39 +0100

    Exchange detail structure starts

--- src/pages/exchanges/Exchange.vue
+++ src/pages/exchanges/Exchange.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
-    <h2>Exchange: {{ id }}</h2>
-    {{ exchange.name}}
-    {{ exchange.code}}
-    {{ exchange.description}}
-    {{ exchange.logo}}
-    {{ exchange.country}}
+  <div class="row">
+    <q-card class="my-card">
+      <q-card-section>
+        <q-img :src="exchange.logo" style="max-width: 400px; height: 200px;">
+          <div class="absolute-bottom text-subtitle1 text-center">{{ exchange.name}}</div>
+        </q-img>
+
+        <h6>{{ exchange.code}}</h6>
+
+        <div>{{ exchange.description}}</div>
+      </q-card-section>
+    </q-card>
+    <q-card>
+      <q-card-section>
+        <p>
+          Accounts:
+          <b>{{ exchange.accounts}}</b>
+        </p>
+        <p>
+          Location:
+          <b>{{ exchange.location}}</b>
+        </p>
+      </q-card-section>
+    </q-card>
   </div>
 </template>
 
@@ -30,3 +47,9 @@ export default Vue.extend({
   }
 });
 </script>
+<style>
+.q-card {
+  width: 300px;
+  box-shadow: none;
+}
+</style>
