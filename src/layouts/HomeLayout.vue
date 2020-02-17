0f252f6ab95f0f43d7b533f0bcdfca81eef77248
edumag <edu@lesolivex.com>
2020-02-17 20:55:53 +0100

    Start home page

---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+/**
+ * Layout base con menú lateral.
+ */
+export default Vue.extend({
+  name: 'HomeLayout'
+});
+</script>
+<style scope>
+body {
+  background: rgba(221, 75, 57, 0.3) url('~assets/home_background-700.jpg')
+    no-repeat center center fixed;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.5) 0%,
+      rgba(0, 0, 0, 0.5) 100%
+    ),
+    url('~assets/home_background-700.jpg');
+}
+</style>
