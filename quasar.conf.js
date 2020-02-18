28ef1bcfe9140a89ac4ccfd1af41b46371604024
edumag <edu@lesolivex.com>
2020-02-18 19:54:46 +0100

    Add Notify and Vuelidate

--- quasar.conf.js
+++ quasar.conf.js
@@ -42,7 +42,12 @@ module.exports = function(ctx) {
       directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: ['Notify'],
+      config: {
+        notify: {
+          /* Notify defaults */
+        }
+      }
     },
 
     // https://quasar.dev/quasar-cli/cli-documentation/supporting-ie
