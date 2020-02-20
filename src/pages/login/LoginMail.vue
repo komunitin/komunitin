23a49676463fcb954db6f493cb095bf220e2b1d1
edumag <edu@lesolivex.com>
2020-02-20 14:55:57 +0100

    Polishing css

--- src/pages/login/LoginMail.vue
+++ src/pages/login/LoginMail.vue
@@ -1,39 +1,53 @@
 <template>
-  <div id="login-mail">
-    <form @submit.prevent="submit">
-      <q-input
-        v-model="mail"
-        type="email"
-        placeholder="example@example.com"
-        label="E-mail"
-        :error="$v.mail.$invalid"
-        counter
-        color="white"
-        labelColor="white"
-        text-color="white"
-        maxlength="30"
-      />
-      <q-input
-        v-model="pass"
-        type="password"
-        label="Contraseña"
-        color="white"
-        :error="$v.pass.$invalid"
-        counter
-        maxlength="10"
-        labelColor="white"
-        text-color="white"
-      />
+  <div class="home-body">
+    <q-toolbar>
       <q-btn
-        outline
-        color="transparent"
-        text-color="white"
-        :label="$t('Login')"
-        icon="account_circle"
-        :disabled="submitStatus === 'PENDING'"
-        type="submit"
+        flat
+        dense
+        round
+        color="white"
+        icon="arrow_back"
+        aria-label="Home"
+        @click="$router.back()"
       />
-    </form>
+      <q-toolbar-title class="text-white">Komunitin</q-toolbar-title>
+    </q-toolbar>
+    <div id="login-mail" class="home-body">
+      <form @submit.prevent="submit">
+        <q-input
+          v-model="mail"
+          type="email"
+          placeholder="example@example.com"
+          label="E-mail"
+          :error="$v.mail.$invalid"
+          counter
+          color="white"
+          labelColor="white"
+          text-color="white"
+          maxlength="30"
+        />
+        <q-input
+          v-model="pass"
+          type="password"
+          label="Contraseña"
+          color="white"
+          :error="$v.pass.$invalid"
+          counter
+          maxlength="10"
+          labelColor="white"
+          text-color="white"
+        />
+        <q-btn
+          outline
+          color="transparent"
+          text-color="white"
+          :label="$t('Login')"
+          icon="account_circle"
+          :disabled="submitStatus === 'PENDING'"
+          type="submit"
+        />
+      </form>
+    </div>
   </div>
 </template>
 
@@ -71,7 +85,7 @@ export default Vue.extend({
         console.log(this.$v);
         this.$q.notify({
           type: 'negative',
-          message: `No se ha podido validar el formulario.`
+          message: 'No se ha podido validar el formulario.'
         });
       } else {
         // @todo do your submit logic here
@@ -85,7 +99,7 @@ export default Vue.extend({
   }
 });
 </script>
-<style lang="stylus">
+<style scope>
 #login-mail {
   width: 300px;
   display: inline-block;
