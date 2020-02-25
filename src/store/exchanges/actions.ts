84c015174ea74cf9cef2acd3a4cb4cebbe6860da
edumag <edu@lesolivex.com>
2020-02-25 10:49:38 +0100

    Mock from exchange

--- src/store/exchanges/actions.ts
+++ src/store/exchanges/actions.ts
@@ -1,5 +1,3 @@
-// import axios from 'axios';
-// import { mockExchange, mockExchangesList } from './mockData';
 import api from '../../services/ICESApi';
 
 export function clearLastError({ commit }: any) {
@@ -7,24 +5,25 @@ export function clearLastError({ commit }: any) {
 }
 
 export function getAllExchanges({ commit }: any) {
-  // @todo llamada a la API o devolvemos Mock dependiendo de la configuración.
   api
     .getExchangesList()
     .then(response => {
       commit('collectExchanges', response.data);
     })
     .catch(function(error) {
-      // console.log({ ERROR: error });
       commit('setLastError', error);
       // throw error;
     });
-  // commit('collectExchanges', mockExchangesList);
 }
 
 export function getExchange({ commit }: any, id: number) {
-  // @todo llamada a la API o devolvemos Mock dependiendo de la configuración.
-  api.getExchange(id).then(response => {
-    commit('collectExchanges', response.data);
-  });
-  // commit('getExchange', mockExchange.data);
+  api
+    .getExchange(id)
+    .then(response => {
+      commit('getExchange', response.data);
+    })
+    .catch(function(error) {
+      commit('setLastError', error);
+      // throw error;
+    });
 }
