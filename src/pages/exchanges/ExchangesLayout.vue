7fedacf4b71d1c5c19c6e0d3ce4b8d56fd1c9946
edumag <edu@lesolivex.com>
2020-02-28 23:33:06 +0100

    Search input

--- src/pages/exchanges/ExchangesLayout.vue
+++ src/pages/exchanges/ExchangesLayout.vue
@@ -1,38 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          v-if="$router.currentRoute.path == '/'"
-          flat
-          dense
-          round
-          @click="leftDrawerOpen = !leftDrawerOpen"
-          icon="menu"
-          aria-label="Menu"
-        />
-
-        <q-btn
-          v-else
-          flat
-          dense
-          round
-          icon="arrow_back"
-          aria-label="Home"
-          @click="$router.back()"
-        />
-
-        <q-toolbar-title>Komunitin</q-toolbar-title>
-
-        <div>
-          <q-btn flat dense round icon="share" aria-label="Share" />
-          <q-btn flat dense round icon="account_circle" aria-label="Account" />
-        </div>
-      </q-toolbar>
-    </q-header>
-    <q-page-container class="container-kn">
-      <router-view />
-    </q-page-container>
+    <router-view />
   </q-layout>
 </template>
 
@@ -56,5 +24,14 @@ body {
   background-color: rgb(255, 255, 255, 1) !important;
   color: black !important;
   background-image: none !important;
+}
+.q-header {
+  z-index: 3001;
+}
+.col-kn {
+  margin-top: 0px;
+}
+.card-kn {
+  width: 100%;
 }
 </style>
