a59de581e41aef7675992f5a876b952f6bbdd478
edumag <edu@lesolivex.com>
2020-03-03 21:37:09 +0100

    Random content for groups list

--- src/pages/exchanges/models/mockData.ts
+++ src/pages/exchanges/models/mockData.ts
@@ -1,83 +1,51 @@
 import { GroupsListModel, GroupModel } from './model';
+import { LoremIpsum } from 'lorem-ipsum';
 
-export const mockGroupsList: GroupsListModel[] = [
-  {
-    id: 1,
-    name: 'Echange 1',
-    code: 'EXEM',
-    description: `
-      Aliqua sint adipisicing cillum deserunt quis. Consequat magna dolor eiusmod ea fugiat duis cupidatat. Quis nulla amet magna dolore et irure excepteur.    
-    `,
-    accounts: 120,
-    logo:
-      'https://www.deluxe.com/sites/www.deluxe.com/files/logo_design/logo/mystic-mountain-300x300.png',
-    attributes: {
-      location: {
-        name: 'Cremallera de Montserrat',
-        type: 'Point',
-        coordinates: [41.5922793, 1.8342942]
-      }
-    }
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
   },
-  {
-    id: 2,
-    name: 'Echange 2',
-    code: 'EXE2',
-    description: `
-      Ut proident sit sint anim exercitation veniam duis occaecat fugiat exercitation exercitation qui ea eiusmod. Veniam dolor reprehenderit dolore culpa duis laboris tempor ad enim aliquip eu id ut ex. Eiusmod ad sit veniam mollit reprehenderit sint quis dolore qui ullamco sunt qui et. Aute esse officia excepteur exercitation duis aliquip aute ex in aute. Cillum excepteur eu deserunt consectetur adipisicing. Ipsum ex cupidatat pariatur dolore cupidatat commodo ut.
-
-      Do voluptate aliquip nisi nisi et. Cillum magna do ea officia enim consectetur. In occaecat aliqua dolore elit.
-
-      Dolore commodo proident anim anim eiusmod sint. Elit laboris irure enim duis laborum culpa ea excepteur excepteur. Amet occaecat incididunt Lorem in mollit excepteur do aliquip. Id sint reprehenderit ut aliquip anim irure. Eu reprehenderit laborum cupidatat commodo pariatur anim aliqua excepteur ad in excepteur exercitation.
-    `,
-    accounts: 150,
-    attributes: {
-      location: {
-        name: 'Torre Eiffel',
-        type: 'Point',
-        coordinates: [48.8583736, 2.2922873]
-      }
-    },
-    logo:
-      'https://www.deluxe.com/sites/www.deluxe.com/files/logo_design/logo/colorpro.png'
-  },
-  {
-    id: 3,
-    name: 'Echange 3',
-    code: 'EXE3',
-    description: `
-      Aliqua sint adipisicing cillum deserunt quis. Consequat magna dolor eiusmod ea fugiat duis cupidatat. Quis nulla amet magna dolore et irure excepteur.    
-    `,
-    accounts: 620,
-    attributes: {
-      location: {
-        name: 'Les Olives',
-        type: 'Point',
-        coordinates: [42.1050622, 3.0157955]
-      }
-    },
-    logo:
-      'https://www.deluxe.com/sites/www.deluxe.com/files/logo_design/logo/femme-foto-300x300.png'
-  },
-  {
-    id: 4,
-    name: 'Echange 4',
-    code: 'EXE4',
-    description: `
-      Aliqua sint adipisicing cillum deserunt quis. Consequat magna dolor eiusmod ea fugiat duis cupidatat. Quis nulla amet magna dolore et irure excepteur.    
-    `,
-    accounts: 120,
-    attributes: {
-      location: {
-        name: 'Playa de La Concha',
-        type: 'Point',
-        coordinates: [43.3178579, -1.9882534]
-      }
-    },
-    logo:
-      'https://www.deluxe.com/sites/www.deluxe.com/files/logo_design/logo/whatcom.png'
+  wordsPerSentence: {
+    max: 16,
+    min: 4
   }
-];
+});
+
+// lorem.generateWords(1);
+// lorem.generateSentences(5);
+// lorem.generateParagraphs(7);
+
+export function mockGroupsList(): GroupsListModel[] {
+  const list = [];
+
+  for (let index = 0; index < 11; index++) {
+    list.push({
+      id: index,
+      data: {
+        attributes: {
+          code: 'COD' + index,
+          name: lorem.generateWords(Math.round(Math.random() * 2) + 1),
+          description: lorem.generateParagraphs(
+            Math.round(Math.random() * 4) + 1
+          ),
+          image:
+            'https://www.deluxe.com/sites/www.deluxe.com/files/logo_design/logo/mystic-mountain-300x300.png',
+          website: 'https://easterislandexchange.org',
+          access: 'public',
+          location: {
+            name: 'Cremallera de Montserrat',
+            type: 'Point',
+            coordinates: [41.5922793, 1.8342942]
+          }
+        }
+      }
+    });
+  }
+  console.log(list);
+  // @ts-ignore
+  return list;
+}
 
 export const mockGroup: GroupModel[] = [
   {
