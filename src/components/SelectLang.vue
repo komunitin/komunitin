8cbf2e8efa96ee6d3f526dbb1ab1309e74f9c00d
Esteve <badia.orive@gmail.com>
2020-03-10 17:30:59 +0100

    WIP - simplify footer structure

--- src/components/SelectLang.vue
+++ src/components/SelectLang.vue
@@ -1,17 +1,14 @@
 <template>
-  <q-select
-    class="gray-kn"
-    flat
-    dense
-    round
-    outlined
-    @input="setLocale"
-    v-model="locale"
-    display-value="Languages"
-    emit-value
-    map-options
-    :options="langs"
-  />
+  <q-btn-dropdown label="Language">
+    <q-list>
+       <q-item v-for="lang in langs" :key="lang.value" clickable v-close-popup @click="setLocale">
+          <q-item-section>
+            <q-item-label>{{ lang.label }}</q-item-label>
+          </q-item-section>
+        </q-item>
+       </q-item>
+    </q-list>
+  </q-btn-dropdown>
 </template>
 <!--
 -->
