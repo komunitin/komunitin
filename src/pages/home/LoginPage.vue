7c124430d43d5106bfbaeff946b9b32d39d4b568
Esteve <badia.orive@gmail.com>
2020-03-12 19:47:32 +0100

    Refactoring CSS - WIP
    Done home page.

--- src/pages/home/LoginPage.vue
+++ src/pages/home/LoginPage.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="home-body">
   <div class="home-center">
-    <div class="home-title">
-      <h1>
-        {{ $t('Welcome to') }}
-        <br />
-        <span>Komunitin</span>
+    <div class="text-onoutside">
+      <h1 class="q-mb-xs">
+        <div id="welcome-to">{{ $t('Welcome to') }}</div>
+        <div id="komunitin">Komunitin</div>
       </h1>
-      <h6>{{ $t('Open System for Exchange Communities') }}</h6>
+      <p id="slogan" class="text-subtitle1">{{ $t('Open System for Exchange Communities') }}</p>
     </div>
     <div id="login-home">
       <q-btn
+        unelevated
         color="primary"
         text-color="onprimary"
         :label="$t('Find your local exchange')"
         icon="explore"
         :to="{ name: 'ExchangesListPage' }"
+        class="full-width q-mt-xl"
+        id="explore"
       />
-      <p class="home-text">{{ $t('Already registered to a local exchange?') }}</p>
+      <p class="text-onoutside-m q-mt-md q-mb-xs">{{ $t('Already registered to a local exchange?') }}</p>
       <q-btn
         outline
         color="transparent"
-        text-color="onprimary"
+        text-color="onoutside"
         :label="$t('Login')"
         :to="{ name: 'LoginSelect' }"
         icon="account_circle"
+        class="full-width"
       />
     </div>
   </div>
@@ -44,33 +47,26 @@ export default Vue.extend({
 });
 </script>
 <style lang="scss" scope>
-.home-title {
-  margin: 30px 0;
+
+#welcome-to {
+  font-weight: 300;
+  font-size: 4.1rem;
+  line-height: 4.1rem;
 }
-.home-title h1 {
-  font-family: roboto;
-  font-size: 40px;
-  text-align: center;
-  margin: 35px 0 0 0;
-  line-height: 49px;
+
+#komunitin {
+  font-weight: 500;
+  font-size: 4.65rem;
+  line-height: 4.65rem;
 }
-.home-title h1 span {
-  font-weight: bold;
-  font-size: 46px;
-}
-.home-title h6 {
-  font-family: roboto;
+
+#slogan {
   font-style: italic;
-  text-align: center;
-  font-size: 11px;
-  margin: 0;
+  font-size: 1.09rem;
 }
-.home-body .q-btn {
-  width: 300px;
-}
-.home-text {
-  color: $onprimary-medium;
-  margin: 16px 0 4px 0;
+
+#explore {
+  height: 48px;
 }
 </style>
 
