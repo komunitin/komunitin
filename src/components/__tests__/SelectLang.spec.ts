5b473a51578214e30d9ee3ffd3898f55f3251709
edumag <edu@lesolivex.com>
2020-03-13 17:09:33 +0100

    Added support to debug test with jest

--- src/components/__tests__/SelectLang.spec.ts
+++ src/components/__tests__/SelectLang.spec.ts
@@ -1,6 +1,14 @@
 import { createLocalVue, mount, Wrapper } from '@vue/test-utils';
 import SelectLang from '../SelectLang.vue';
-import { Quasar, QSelect } from 'quasar';
+import {
+  Quasar,
+  QSelect,
+  QItem,
+  QItemSection,
+  QItemLabel,
+  QBtnDropdown,
+  QList
+} from 'quasar';
 
 describe('SelectLang', () => {
   let locale: string;
@@ -13,7 +21,14 @@ describe('SelectLang', () => {
 
   // We need to explicitely include the components to be used.
   localVue.use(Quasar, {
-    components: { QSelect }
+    components: {
+      QSelect,
+      QItem,
+      QItemSection,
+      QItemLabel,
+      QBtnDropdown,
+      QList
+    }
   });
 
   // Montamos el componente con los props necesarios antes de cada test.
@@ -51,11 +66,11 @@ describe('SelectLang', () => {
   it('Check that it emits the selected language', async () => {
     // wrapper.setData({ langs: langs });
     // console.debug({ Test: wrapper.html() });
-    const select = wrapper.find('input');
+    // const select = wrapper.find('input');
     // New lang.
     const newLang = wrapper.vm.$data.langs[1].value;
     wrapper.setData({ locale: newLang });
-    select.setValue(newLang);
+    // select.setValue(newLang);
     // console.debug({ Test: wrapper.html() });
     wrapper.vm.$emit('setLocale');
     wrapper.vm.$emit('setLocale', newLang);
