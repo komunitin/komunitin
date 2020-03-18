3f6bdba71ad925a790e93ccbc39a043d60992d92
edumag <edu@lesolivex.com>
2020-03-18 17:50:00 +0100

    Cleaning and reconfiguring of lint
    - Allow @ ts-ignore but it gives us a warning.
    - Notices are added for the vue format.

--- src/components/__tests__/SocialButtons.spec.ts
+++ src/components/__tests__/SocialButtons.spec.ts
@@ -1,10 +1,11 @@
-import { createLocalVue, shallowMount, Wrapper } from '@vue/test-utils';
-import SocialButtons from '../SocialButtons.vue';
-import { Quasar } from 'quasar';
+import { createLocalVue, shallowMount, Wrapper } from "@vue/test-utils";
+import SocialButtons from "../SocialButtons.vue";
+import { Quasar } from "quasar";
 
-describe('SocialButtons', () => {
+describe("SocialButtons", () => {
   let url: string;
   let title: string;
+  // @ts-ignore
   let wrapper: Wrapper<SocialButtons>;
 
   // We use createLocalVue in order not to pollute the global scope.
@@ -17,8 +18,8 @@ describe('SocialButtons', () => {
 
   // Montamos el componente con los props necesarios antes de cada test.
   beforeEach(() => {
-    url = 'https://example.com';
-    title = 'Example Title';
+    url = "https://example.com";
+    title = "Example Title";
     wrapper = shallowMount(SocialButtons, {
       propsData: {
         url: url,
@@ -28,7 +29,7 @@ describe('SocialButtons', () => {
     });
   });
 
-  it('Html generated', async () => {
+  it("Html generated", async () => {
     await wrapper.vm.$nextTick();
     // console.debug({ Test: wrapper.html() });
     expect(wrapper.html()).toContain(
