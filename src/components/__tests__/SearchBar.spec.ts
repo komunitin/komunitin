edc7050485d95570031c0ab2396334fccca85bc9
Esteve <badia.orive@gmail.com>
2020-04-10 10:30:01 +0200

    Remove lint warning

--- src/components/__tests__/SearchBar.spec.ts
+++ src/components/__tests__/SearchBar.spec.ts
@@ -5,8 +5,7 @@ import { Quasar, QBtn, QToolbar, QToolbarTitle, QInput } from "quasar";
 describe("SearchBox", () => {
   let title: string;
   let backButton: boolean;
-  // @ts-ignore
-  let wrapper: Wrapper<SearchBar>;
+  let wrapper: Wrapper<Vue>;
 
   // We use createLocalVue in order not to pollute the global scope.
   const localVue = createLocalVue();
