5d3b0f1c8397f83a7cbc62c3ce3111016a756b6e
XaviP <xavip@medusaweb.cat>
2020-04-18 20:28:56 +0200

    language without translation & package-lock.json out of .gitignore

--- src/components/SelectLang.vue
+++ src/components/SelectLang.vue
@@ -1,5 +1,6 @@
 <template>
-  <q-btn-dropdown flat :label="$t('language')">
+  <!-- Don't translate the word "Language" since it will probably be better understood by the user seeking for this option rather than the translated word to a language that the user wants to change. -->
+  <q-btn-dropdown flat label="Language">
     <q-list>
       <q-item
         v-for="lang in langs"
