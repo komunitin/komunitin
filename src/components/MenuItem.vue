ce78d72d4dcd596e1ee6c4b6a530d8c42232e9fa
Esteve <badia.orive@gmail.com>
2020-05-11 12:11:28 +0200

    WIP

---
+++ src/components/MenuItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-item clickable>
+    <q-item-section avatar>
+      <q-icon :name="icon" />
+    </q-item-section>
+    <q-item-label>{{ title }}</q-item-label>
+  </q-item>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+/**
+ * List item in the left drawer menu.
+ */
+export default Vue.extend({
+  name: "menu-item",
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  }
+});
+</script>
---
+++ src/layouts/User.vue
@@ -0,0 +1,19 @@
+<template>
+    <q-layout view="lhh Lpr lff">
+        <q-drawer show-if-above v-model="left" side="left" bordered>
+            <menu-drawer/>
+        </q-drawer>
+        <router-view/>
+    </q-layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import menuDrawer from '../components/MenuDrawer.vue';
+/**
+ * Main layout with left drawer, toolbar and main content.
+ */
+export default Vue.extend({
+  name: 'User'
+});
+</script>
