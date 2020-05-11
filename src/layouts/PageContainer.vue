94594c0f44829be5ed70c2a6e5c72d8ff84d835f
Esteve <badia.orive@gmail.com>
2020-05-11 12:11:28 +0200

    WIP

---
+++ src/layouts/PageContainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <q-header reveal bordered>
+        <slot name="toolbar">
+            <q-toolbar>
+            <q-btn flat round icon="menu" @click="left = !left" />
+            <q-toolbar-title>
+                Title
+            </q-toolbar-title>
+            </q-toolbar>
+        </slot>
+    </q-header>
+    <q-page-container>
+        <slot/>
+    </q-page-container>
+  </div>
+</template>
