eff24ceee295523358b24d7d7c5ab964497e7e5b
Esteve <badia.orive@gmail.com>
2020-05-20 15:23:56 +0200

    Test logout

--- test/jest/__tests__/Login.spec.ts
+++ test/jest/__tests__/Login.spec.ts
@@ -4,6 +4,8 @@
 import { Wrapper } from "@vue/test-utils";
 import App from "../../../src/App.vue";
 import { mountComponent } from "../utils";
+import MenuDrawer from "../../../src/components/MenuDrawer.vue";
+import MenuItem from "../../../src/components/MenuItem.vue";
 
 describe("Front page and login", () => {
   let wrapper: Wrapper<Vue>;
@@ -43,20 +45,38 @@ describe("Front page and login", () => {
     expect(wrapper.vm.$route.path).toBe("/");
   });
 
-  it("logs in with email and password", async () => {
+  it("login and logout", async () => {
     expect(wrapper.vm.$store.getters.isLoggedIn).toBe(false);
     // Go to login with mail page.
     wrapper.vm.$router.push("/login-mail");
     await wrapper.vm.$nextTicks();
     // Button is disabled since form is empty.
-    expect(wrapper.find("button[type='submit']").attributes().disabled).toBe("disabled");
+    expect(wrapper.find("button[type='submit']").attributes().disabled)
+      .toBe("disabled");
     wrapper.find("input[type='email']").setValue("example@example.com");
     wrapper.find("input[type='password']").setValue("password");
     await wrapper.vm.$nextTick();
     // Button is enabled now.
-    expect(wrapper.find("button[type='submit']").attributes().disabled).toBeUndefined();
+    expect(
+      wrapper.find("button[type='submit']").attributes().disabled
+    ).toBeUndefined();
     wrapper.find("button[type='submit']").trigger("click");
     await wrapper.vm.$wait();
     expect(wrapper.vm.$store.getters.isLoggedIn).toBe(true);
+    expect(wrapper.vm.$route.path).toBe("/groups/GRP0");
+    // Click the account switcher
+    wrapper
+      .find("#my-member")
+      .find("button")
+      .trigger("click");
+    await wrapper.vm.$nextTick();
+    // Click logout
+    wrapper
+      .find(MenuDrawer)
+      .find({ ref: "logout" })
+      .trigger("click");
+    await wrapper.vm.$wait();
+    expect(wrapper.vm.$store.getters.isLoggedIn).toBe(false);
+    expect(wrapper.vm.$route.path).toBe("/");
   });
 });
