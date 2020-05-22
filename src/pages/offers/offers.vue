7802d342feb6b2ea69e9f77784973e0cee0bedc4
Esteve <badia.orive@gmail.com>
2020-05-22 20:01:23 +0200

    WIP

---
+++ src/pages/offers/offers.vue
@@ -0,0 +1,19 @@
+<template>
+  <layout :search="true" :title="$t('offers')" @search="fetchOffers">
+
+  </layout>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  methods: {
+    fetchOffers(search?: string) {
+      this.$store.dispatch("offers/loadList", {
+        location: this.location,
+        search,
+        include: "members"
+      });
+    }
+  }
+})
+</script>
