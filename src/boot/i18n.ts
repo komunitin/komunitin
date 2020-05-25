95e47c8b0b59a812eb7079255a61f37ca308c32e
Esteve <badia.orive@gmail.com>
2020-05-25 13:03:17 +0200

    Format offer date

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,35 +1,43 @@
 import Vue from "vue";
-import { boot } from 'quasar/wrappers'
+import { boot } from "quasar/wrappers";
 import VueI18n from "vue-i18n";
-import DefaultMessages from 'src/i18n/en-us/index.json';
-import { KOptions } from './komunitin';
-import { LocalStorage, QVueGlobals } from 'quasar';
+import DefaultMessages from "src/i18n/en-us/index.json";
+import { KOptions } from "./komunitin";
+import { LocalStorage, QVueGlobals } from "quasar";
+import { formatRelative, Locale } from "date-fns";
 
 /**
  * Default to english language.
  */
 const DEFAULT_LANG = "en-us";
-/** 
+/**
  * LocalStorage key for the saved locale.
  */
 const LOCALE_KEY = "lang";
 
-/** 
+/**
+ * THe current date locale.
+ */
+let dateLocale = undefined as Locale | undefined;
+
+/**
  * VueI18n instance with default locale preloaded.
- * 
+ *
  * Important: Call the boot function before using this variable!
  */
 export let i18n: VueI18n;
 
 /**
- * Return locale if it is a defined language for this app, 
+ * Return locale if it is a defined language for this app,
  * or the default language code (English) instead.
  * **/
-function normalizeLocale(locale: string) : string {
+function normalizeLocale(locale: string): string {
   return KOptions.langs.reduce(
-    (lang: string, elem: {label: string, value: string}) => {
-      return elem.value === locale ? locale : lang
-    }, DEFAULT_LANG); 
+    (lang: string, elem: { label: string; value: string }) => {
+      return elem.value === locale ? locale : lang;
+    },
+    DEFAULT_LANG
+  );
 }
 
 /**
@@ -50,7 +58,7 @@ function getCurrentLocale($q: QVueGlobals): string {
 async function setI18nLocale(locale: string) {
   if (i18n.locale !== locale) {
     const messages = (await import(`src/i18n/${locale}`)).default;
-    i18n.setLocaleMessage(locale, messages)
+    i18n.setLocaleMessage(locale, messages);
     i18n.locale = locale;
   }
 }
@@ -61,12 +69,22 @@ async function setQuasarLang($q: QVueGlobals, lang: string) {
   $q.lang.set(messages);
 }
 
+async function setDateLocale(locale: string) {
+  if (locale.includes("-")) {
+    locale = locale.split("-",2).map((value, index) => (index > 0 ? value.toUpperCase() : value)).join("-");
+  }
+  dateLocale = (await import(`date-fns/locale/${locale}/index.js`)).default;
+}
 /**
  * Asynchronously sets the current locale.
  * **/
 async function setLocale($q: QVueGlobals, locale: string) {
   const lang = normalizeLocale(locale);
-  await Promise.all([setI18nLocale(lang), setQuasarLang($q, lang)]);
+  await Promise.all([
+    setI18nLocale(lang),
+    setQuasarLang($q, lang),
+    setDateLocale(lang)
+  ]);
   localStorage.setItem(LOCALE_KEY, locale);
 }
 
@@ -74,13 +92,13 @@ async function setLocale($q: QVueGlobals, locale: string) {
 export default boot(async ({ app, Vue }) => {
   // Install 'vue-i18n' plugin.
   Vue.use(VueI18n);
-  
+
   i18n = new VueI18n({
     locale: DEFAULT_LANG,
     fallbackLocale: DEFAULT_LANG,
     messages: {
-      [DEFAULT_LANG] : DefaultMessages
-    }    
+      [DEFAULT_LANG]: DefaultMessages
+    }
   });
   // Add $i18n variable in app.
   app.i18n = i18n;
@@ -93,12 +111,19 @@ export default boot(async ({ app, Vue }) => {
   Vue.prototype.$setLocale = async function(locale: string) {
     // Get Quasar from this Vue instance.
     await setLocale(this.$q, locale);
-  }
+  };
 
+  // Add date filter to Vue.
+  Vue.filter("date", (date: string) =>
+    formatRelative(new Date(date), new Date(), {
+      locale: dateLocale
+    })
+  );
 });
 
-declare module 'vue/types/vue' {
+declare module "vue/types/vue" {
   interface Vue {
     $setLocale: (locale: string) => Promise<void>;
+    $formatDate: (date: Date) => string;
   }
 }
