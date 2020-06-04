b8a3bcc16ad5dbc7206c79b2ad2ec5858720a36f
Esteve <badia.orive@gmail.com>
2020-06-04 19:43:07 +0200

    Member & Account APIs

--- src/store/resources.ts
+++ src/store/resources.ts
@@ -9,7 +9,7 @@ import {
 import Axios, { AxiosInstance, AxiosError, AxiosResponse } from "axios";
 import KError, { KErrorCode } from "src/KError";
 
-interface ResourcesState<T extends ResourceObject> {
+export interface ResourcesState<T extends ResourceObject> {
   /**
    * Dictionary of resources indexed by id.
    */
@@ -93,12 +93,12 @@ export interface LoadUrlPayload {
  * Flexible Vuex Module used to retrieve, store and provide resources fetched from the
  * Komunitin Apis. The implementation is generic for any resource thanks to the JSONAPI
  * spec.
- * 
+ *
  * Getters:
  * - `current`: Gives the current resource.
  * - `currentList`: Gives the cirrent list of resources. For paginated collections, this is the current chunk.
  * - `one`: Gives a resource given its id.
- * 
+ *
  * Actions:
  * - `load`: Fetches the current resource. Accepts the `LoadPayload` argument.
  * - `loadList`: Fetches the current list of resources. Accepts the `LoadListPayload` argument.
@@ -124,7 +124,7 @@ export class Resources<T extends ResourceObject, S> implements Module<ResourcesS
   /**
    * Endpoint for the collection of resources of this type. Override if
    * your resource doesn't follow the standard `/{groupCode}/{type}`.
-   * 
+   *
    * @param groupCode The code of the group
    */
   protected collectionEndpoint(groupCode: string) {
@@ -137,7 +137,7 @@ export class Resources<T extends ResourceObject, S> implements Module<ResourcesS
    * ```
    *  collectionEndpoint(groupCode)/{code}.
    * ```
-   * 
+   *
    * @param code The code of the resource
    * @param groupCode The code of the group
    */
@@ -158,15 +158,20 @@ export class Resources<T extends ResourceObject, S> implements Module<ResourcesS
 
   /**
    * Trigger `add` commit to the relevant modules for each resource given.
-   * 
+   *
    * @param included Included resources
    * @param commit Commit function
    */
   protected static handleIncluded(included: ResourceObject[], commit: Commit) {
-    included.forEach(resource => commit(resource.type + "/add", resource, {root: true}));
+    included.forEach(resource =>
+      commit(resource.type + "/add", resource, { root: true })
+    );
   }
 
-  protected handleCollectionResponse(response: AxiosResponse<CollectionResponseInclude<T, ResourceObject>>, commit: Commit) {
+  protected handleCollectionResponse(
+    response: AxiosResponse<CollectionResponseInclude<T, ResourceObject>>,
+    commit: Commit
+  ) {
     // Commit mutation(s).
     const result = response.data;
     commit("setList", result.data);
@@ -178,7 +183,7 @@ export class Resources<T extends ResourceObject, S> implements Module<ResourcesS
 
   /**
    * Handle error from the API.
-   * 
+   *
    * @param error The Network error.
    */
   protected static getKError(error: AxiosError<ErrorObject>) {
@@ -190,7 +195,7 @@ export class Resources<T extends ResourceObject, S> implements Module<ResourcesS
         apiError.code in KErrorCode
           ? (apiError.code as KErrorCode)
           : KErrorCode.UnknownServer;
-  
+
       return new KError(code, apiError.title, error);
     } else if (error.request) {
       // Server didn't respond.
@@ -203,18 +208,22 @@ export class Resources<T extends ResourceObject, S> implements Module<ResourcesS
 
   /**
    * Foreach relationship in main resource with linkage data, add a property of the same
-   * name to the object with the value of the linked resource(s). This is done through a 
+   * name to the object with the value of the linked resource(s). This is done through a
    * getter that lazily loads related resources from the store.
-   * 
+   *
    * @param rootGetters Store getters object.
    * @param main ResourceObject.
-   * 
+   *
    * @return the main ResourceObject
    */
-  protected relatedGetters(rootGetters: Record<string, (id: string) => ResourceObject>, main: ResourceObject): ResourceObject {
+  protected relatedGetters(
+    rootGetters: Record<string, (id: string) => T>,
+    main: T
+  ): T {
     if (main.relationships) {
-      const relationships = Object.entries(main.relationships)
-        .filter(([,value]) => value.data !== undefined);
+      const relationships = Object.entries(main.relationships).filter(
+        ([, value]) => value.data !== undefined
+      );
       if (relationships.length > 0) {
         // Copy object so we don't modify the object in state.
         main = JSON.parse(JSON.stringify(main));
@@ -222,12 +231,13 @@ export class Resources<T extends ResourceObject, S> implements Module<ResourcesS
           if (Array.isArray(value.data)) {
             Object.defineProperty(main, name, {
               get: function() {
-                return (value.data as ResourceIdentifierObject[])
-                  .map(resourceId => rootGetters[resourceId.type + "/one"](resourceId.id));
+                return (value.data as ResourceIdentifierObject[]).map(
+                  resourceId =>
+                    rootGetters[resourceId.type + "/one"](resourceId.id)
+                );
               }
             });
-          }
-          else {
+          } else {
             Object.defineProperty(main, name, {
               get: function() {
                 const resourceId = value.data as ResourceIdentifierObject;
@@ -245,29 +255,48 @@ export class Resources<T extends ResourceObject, S> implements Module<ResourcesS
     resources: {},
     current: null,
     currentList: [],
-    next: undefined,
-  };
+    next: undefined
+  } as ResourcesState<T>;
 
   getters = {
     /**
      * Gets a resource given its id.
      */
-    one: (state: ResourcesState<T>, _getters: unknown, _rootState: unknown, rootGetters: Record<string, (id: string) => ResourceObject>) => (id: string) =>
-      this.relatedGetters(rootGetters, state.resources[id]),
+    one: (
+      state: ResourcesState<T>,
+      _getters: unknown,
+      _rootState: unknown,
+      rootGetters: Record<string, (id: string) => T>
+    ) => (id: string) => state.resources[id] ? this.relatedGetters(rootGetters, state.resources[id]) : null,
     /**
      * Gets the current resource.
      */
-    current: (state: ResourcesState<T>, _getters: unknown, _rootState: unknown, rootGetters: Record<string, (id: string) => ResourceObject>) =>
-      state.current != null ? this.relatedGetters(rootGetters, state.resources[state.current]) : undefined,
+    current: (
+      state: ResourcesState<T>,
+      _getters: unknown,
+      _rootState: unknown,
+      rootGetters: Record<string, (id: string) => T>
+    ) =>
+      state.current != null
+        ? this.relatedGetters(rootGetters, state.resources[state.current])
+        : undefined,
     /**
      * Gets the current list of resources.
      */
-    currentList: (state: ResourcesState<T>, _getters: unknown, _rootState: unknown, rootGetters: Record<string, (id: string) => ResourceObject>) =>
-      state.currentList.map(id => state.resources[id]).map(resource => this.relatedGetters(rootGetters, resource)),
+    currentList: (
+      state: ResourcesState<T>,
+      _getters: unknown,
+      _rootState: unknown,
+      rootGetters: Record<string, (id: string) => T>
+    ) =>
+      state.currentList
+        .map(id => state.resources[id])
+        .map(resource => this.relatedGetters(rootGetters, resource)),
     /**
      * Returns whether the current list has more resources to be fetched.
      */
-    hasNext: (state: ResourcesState<T>) => (state.next === undefined ?  undefined : (state.next !== null)),
+    hasNext: (state: ResourcesState<T>) =>
+      state.next === undefined ? undefined : state.next !== null
   };
 
   mutations = {
@@ -296,112 +325,124 @@ export class Resources<T extends ResourceObject, S> implements Module<ResourcesS
      */
     setNext(state: ResourcesState<T>, next: string | null) {
       state.next = next;
-    },
-  };
-
-  actions = {
-    /**
-     * Fetches the current list of resources.
-     * 
-     * @param payload Configure the request by filtering the results, including related resources, including the current location, sorting.
-     */
-    loadList: async (
-      { commit }: ActionContext<ResourcesState<T>, S>,
-      payload: LoadListPayload
-    ) => {
-      // Build query string.
-      const params = new URLSearchParams();
-      if (payload.search) {
-        params.set("filter[search]", payload.search);
-      }
-      if (payload.filter) {
-        Object.entries(payload.filter).map(([field, value]) => {
-          params.set(`filter[${field}]`, value);
-        });
-      }
-      if (payload.location) {
-        params.set(
-          "geo-position",
-          payload.location[0] + "," + payload.location[1]
-        );
-        if (!payload.sort) {
-          payload.sort = "location";
-        }
-      }
-      if (payload.sort) {
-        params.set("sort", payload.sort);
-      }
-      if (payload.include) {
-        params.set("include", payload.include);
-      }
-      let url = this.collectionEndpoint(payload.group);
-      const query = params.toString();
-      if (query.length > 0) url += "?" + query;
-      // Call API
-      try {
-        const response = await this.client.get<CollectionResponseInclude<T, ResourceObject>>(url);
-        this.handleCollectionResponse(response, commit);
-      } catch (error) {
-        throw Resources.getKError(error);
-      }
-    },
-    loadNext: async ({ state, commit }: ActionContext<ResourcesState<T>, S>) => {
-      try {
-        if (state.next === null) {
-          // There are no more results.
-          commit("setList", []);
-          return;
-        }
-        if (state.next === undefined) {
-          // We can't do the loadNext call yet, since we don't have any next link. A loadList action should be called first.
-          throw new KError(KErrorCode.ScriptError, "Unexpected call to 'loadNext' resource action with undefined next link.");
-        }
-        const response = await this.client.get<CollectionResponseInclude<T, ResourceObject>>(state.next);
-        this.handleCollectionResponse(response, commit);
-      } catch (error) {
-        throw Resources.getKError(error);
-      }
-    },
-    /**
-     * Fetches the current reaource.
-     */
-    load: async (
-      { commit }: ActionContext<ResourcesState<T>, S>,
-      payload: LoadPayload
-    ) => {
-      let url = this.resourceEndpoint(payload.code, payload.group);
-      if (payload.include) {
-        const params = new URLSearchParams();
-        params.set("include", payload.include);
-        url += "?" + params.toString();
-      }
-      // Call API
-      try {
-        const response = await this.client.get<ResourceResponseInclude<T, ResourceObject>>(url);
-        // Commit mutation(s).
-        const resource = response.data.data;
-        commit("setCurrent", resource);
-        if (response.data.included) {
-          Resources.handleIncluded(response.data.included, commit);
-        }
-      } catch (error) {
-        throw Resources.getKError(error);
-      }
     }
   };
 
+  actions = {
+    loadList: (context: ActionContext<ResourcesState<T>, S>, payload: LoadListPayload) => this.loadList(context, payload),
+    loadNext: (context: ActionContext<ResourcesState<T>, S>) => this.loadNext(context),
+    load: (context: ActionContext<ResourcesState<T>, S>, payload: LoadPayload) => this.load(context, payload)
+  };
   /**
-   * This is a static action implementation that loads any JSONAPI url and commits the 
+   * Fetches the current list of resources.
+   *
+   * @param payload Configure the request by filtering the results, including related resources, including the current location, sorting.
+   */
+  protected async loadList(
+    { commit }: ActionContext<ResourcesState<T>, S>,
+    payload: LoadListPayload
+  ) {
+    // Build query string.
+    const params = new URLSearchParams();
+    if (payload.search) {
+      params.set("filter[search]", payload.search);
+    }
+    if (payload.filter) {
+      Object.entries(payload.filter).map(([field, value]) => {
+        params.set(`filter[${field}]`, value);
+      });
+    }
+    if (payload.location) {
+      params.set(
+        "geo-position",
+        payload.location[0] + "," + payload.location[1]
+      );
+      if (!payload.sort) {
+        payload.sort = "location";
+      }
+    }
+    if (payload.sort) {
+      params.set("sort", payload.sort);
+    }
+    if (payload.include) {
+      params.set("include", payload.include);
+    }
+    let url = this.collectionEndpoint(payload.group);
+    const query = params.toString();
+    if (query.length > 0) url += "?" + query;
+    // Call API
+    try {
+      const response = await this.client.get<CollectionResponseInclude<T, ResourceObject>>(url);
+      this.handleCollectionResponse(response, commit);
+    } catch (error) {
+      throw Resources.getKError(error);
+    }
+  }
+  protected async loadNext({
+    state,
+    commit
+  }: ActionContext<ResourcesState<T>, S>) {
+    try {
+      if (state.next === null) {
+        // There are no more results.
+        commit("setList", []);
+        return;
+      }
+      if (state.next === undefined) {
+        // We can't do the loadNext call yet, since we don't have any next link. A loadList action should be called first.
+        throw new KError(
+          KErrorCode.ScriptError,
+          "Unexpected call to 'loadNext' resource action with undefined next link."
+        );
+      }
+      const response = await this.client.get<CollectionResponseInclude<T, ResourceObject>>(state.next);
+      this.handleCollectionResponse(response, commit);
+    } catch (error) {
+      throw Resources.getKError(error);
+    }
+  }
+  /**
+   * Fetches the current reaource.
+   */
+  protected async load(
+    { commit }: ActionContext<ResourcesState<T>, S>,
+    payload: LoadPayload
+  ) {
+    let url = this.resourceEndpoint(payload.code, payload.group);
+    if (payload.include) {
+      const params = new URLSearchParams();
+      params.set("include", payload.include);
+      url += "?" + params.toString();
+    }
+    // Call API
+    try {
+      const response = await this.client.get<ResourceResponseInclude<T, ResourceObject>>(url);
+      // Commit mutation(s).
+      const resource = response.data.data;
+      commit("setCurrent", resource);
+      if (response.data.included) {
+        Resources.handleIncluded(response.data.included, commit);
+      }
+    } catch (error) {
+      throw Resources.getKError(error);
+    }
+  }
+
+  /**
+   * This is a static action implementation that loads any JSONAPI url and commits the
    * result to the relevant module instance.
-   * 
+   *
    * It should be added as a single `loadUrl` action implementation.
    */
-  static async loadUrl({commit}: ActionContext<never, never>, payload: LoadUrlPayload) {
+  static async loadUrl(
+    { commit }: ActionContext<never, never>,
+    payload: LoadUrlPayload
+  ) {
     let url = payload.url;
     if (payload.include) {
       const params = new URLSearchParams();
       params.set("include", payload.include);
-      url += url.includes("?") ? "&" : "?"
+      url += url.includes("?") ? "&" : "?";
       url += params.toString();
     }
     try {
@@ -409,19 +450,22 @@ export class Resources<T extends ResourceObject, S> implements Module<ResourcesS
       const response = await client.get<ResourceResponseInclude<ResourceObject, ResourceObject>>(url);
       const resource = response.data.data;
       if (Array.isArray(resource)) {
-        throw new KError(KErrorCode.NotImplemented, "Generic loading of resource collections is not implemented. May be it is a good moment to do so ;)");
+        throw new KError(
+          KErrorCode.NotImplemented,
+          "Generic loading of resource collections is not implemented. May be it is a good moment to do so ;)"
+        );
       }
       const type = resource.type;
-      // Mutate the relevant vuex module, that is by convention named 
+      // Mutate the relevant vuex module, that is by convention named
       // equal to the resource type.
-      commit(`${type}/setCurrent`, resource, {root: true});
+      commit(`${type}/setCurrent`, resource, { root: true });
       if (response.data.included) {
         Resources.handleIncluded(response.data.included, commit);
       }
     } catch (error) {
       throw Resources.getKError(error);
     }
-    // Note that all action implementations are similar, 
+    // Note that all action implementations are similar,
     // so they could be factored as a single generic one.
   }
 }
