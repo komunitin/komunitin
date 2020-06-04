159e607affea19538df7318ff0979cc9868bb804
Esteve <badia.orive@gmail.com>
2020-06-04 19:43:07 +0200

    Member & Account APIs

---
+++ src/server/ServerUtils.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/**
+ * Implements the filtering for a JSON:API request.
+ * 
+ * @param records 
+ * @param request 
+ */
+export function filter(records: any, request: any) {
+  // Poor man search.
+  if (request.queryParams["filter[search]"]) {
+    const fragment = request.queryParams["filter[search]"];
+    records = records.filter((record: any) =>
+      Object.values(record.attrs).some(
+        (value: any) => value && value.toString().toLowerCase().includes(fragment.toLowerCase())
+      )
+    );
+  }
+  // Filter by field values
+  const regex = /filter\[(.+)\]/;
+  Object.entries(request.queryParams)
+    .filter(([name, ]) => name != "filter[search]")
+    .filter(([name, ]) => regex.test(name))
+    .map(([name, value]) => ([(name.match(regex) as string[])[1], value]) as [string,string])
+    .forEach(([name, value] ) => {
+      const values = value.split(",");
+      records = records.filter((record: any) => values.includes(record[name]))
+    });
+    
+  return records;
+}
