85b61fe618518b9f89074ad2d1a5d7a8daca16f1
Esteve <badia.orive@gmail.com>
2020-07-10 17:23:07 +0200

    Fix header test. Fix and improve group test

--- test/jest/__tests__/Groups.spec.ts
+++ test/jest/__tests__/Groups.spec.ts
@@ -26,4 +26,42 @@ describe("Groups", () => {
     expect(wrapper.findAll(GroupCard).length).toBe(7);
     expect((wrapper.find(QInnerLoading).vm as QInnerLoading).showing).toBe(false);
   });
+
+  it ("Render group page", async () => {
+    await wrapper.vm.$router.push("/groups/GRP0");
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find(QInnerLoading).isVisible()).toBe(true);
+    await wrapper.vm.$wait();
+    const text = wrapper.text();
+    // Title
+    expect(text).toContain("Group 0");
+    // Code
+    expect(text).toContain("GRP0");
+    // Description
+    expect(text).toContain("Velit quis voluptatum harum");
+    // URL
+    expect(text).toContain("vance.info");
+    // Offers card
+    expect(text).toContain("Offers");
+    expect(text).toContain("30");
+    expect(text).toContain("6 Garden");
+    // Needs card
+    expect(text).toContain("Needs");
+    expect(text).toContain("4");
+    expect(text).toContain("1 Garden");
+    // Members card
+    expect(text).toContain("Members");
+    expect(text).toContain("30");
+    expect(text).toContain("Explore");
+    // Currency card
+    expect(text).toContain("Currency");
+    expect(text).toContain("$");
+    // Location
+    expect(text).toContain("Avon");
+    // Contact
+    expect(text).toContain("558-336-5373");
+    expect(text).toContain("Mittie.Green@hotmail.com");
+    expect(text).toContain("@Noel82");
+    expect(text).toContain("474-283-1596");
+  })
 });
