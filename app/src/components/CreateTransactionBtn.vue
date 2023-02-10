027183a6d3c3166ccb6b1ca0f677360068a97779
Esteve Badia <badia.orive@gmail.com>
2023-02-10 16:13:29 +0100

    Create CreateTransactionBtn.vue

---
+++ app/src/components/CreateTransactionBtn.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page-sticky
+    position="bottom"
+    :offset="[0, 24]"
+  >
+    <q-btn
+      fab
+      icon="add"
+      color="primary"
+      :label="$t('createTransaction')"
+      :to="`/groups/${myMember.group.attributes.code}/members/${myMember.attributes.code}/transactions/new`"
+    />
+  </q-page-sticky>
+</template>
+<script lang="ts" setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+const myMember = computed(() => store.getters.myMember)
+</script>
