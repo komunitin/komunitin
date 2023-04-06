1c75808d8fb48a401d87f15c04929774c98fa0b1
Esteve Badia <badia.orive@gmail.com>
2023-04-06 13:27:48 +0200

    Location banner

--- app/src/layouts/PageContainer.vue
+++ app/src/layouts/PageContainer.vue
@@ -1,12 +1,19 @@
 <template>
   <q-page-container>
-    <notifications-banner />
+    <notifications-banner 
+      @show-change="notificationsShowChange"
+    />
+    <location-banner 
+      v-if="locationShow"
+      @show-change="locationShowChange"
+    />
     <slot />
   </q-page-container>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue"
+import { defineComponent, computed, ref } from "vue"
 import NotificationsBanner from "../components/NotificationsBanner.vue"
+import LocationBanner from "../components/LocationBanner.vue"
 
 /**
  * This component is a layout utility to correctly place banners without interfering with the
@@ -17,7 +24,19 @@ import NotificationsBanner from "../components/NotificationsBanner.vue"
  */
 export default defineComponent({
   components: {
-    NotificationsBanner
+    NotificationsBanner,
+    LocationBanner
+  },
+  setup() {
+    const notificationsWantShow = ref<boolean|undefined>(undefined)
+    const notificationsShowChange = (show: boolean) => {notificationsWantShow.value = show}
+
+    const locationWantShow = ref<boolean|undefined>(undefined)
+    const locationShowChange = (show: boolean) => {locationWantShow.value = show}
+
+    const locationShow = computed(() => notificationsWantShow.value === false)
+
+    return {locationShowChange, notificationsShowChange, locationShow}
   }
 })
 </script>
