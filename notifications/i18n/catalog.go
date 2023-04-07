1e1e615281afe23ef0230b1209f037d5a521d068
Esteve Badia <badia.orive@gmail.com>
2023-04-07 15:17:56 +0200

    Add texts

--- notifications/i18n/catalog.go
+++ notifications/i18n/catalog.go
@@ -31,7 +31,7 @@ func init() {
 		"en": &dictionary{index: enIndex, data: enData},
 		"es": &dictionary{index: esIndex, data: esData},
 	}
-	fallback := language.MustParse("en-US")
+	fallback := language.MustParse("en")
 	cat, err := catalog.NewFromMap(dict, catalog.Fallback(fallback))
 	if err != nil {
 		panic(err)
@@ -42,35 +42,43 @@ func init() {
 var messageKeyToIndex = map[string]int{
 	"newPurchase":         0,
 	"newPurchaseText":     1,
+	"paymentPending":      4,
+	"paymentPendingText":  5,
 	"paymentReceived":     2,
 	"paymentReceivedText": 3,
 }
 
-var caIndex = []uint32{ // 5 elements
+var caIndex = []uint32{ // 7 elements
 	0x00000000, 0x0000000c, 0x00000025, 0x00000034,
-	0x0000004e,
-} // Size: 44 bytes
+	0x0000004e, 0x00000063, 0x000000cc,
+} // Size: 52 bytes
 
-const caData string = "" + // Size: 78 bytes
+const caData string = "" + // Size: 204 bytes
 	"\x02Nova compra\x02Compra de %[1]v a %[2]v.\x02Pagament rebut\x02Has reb" +
-	"ut %[1]v de %[2]v."
+	"ut %[1]v de %[2]v.\x02Nou pagament pendent\x02Tens un nou pagament de %[" +
+	"1]v per a %[2]v. Sisplau accepta o rebutja el pagament el més aviat poss" +
+	"ible."
 
-var enIndex = []uint32{ // 5 elements
+var enIndex = []uint32{ // 7 elements
 	0x00000000, 0x0000000d, 0x0000002b, 0x0000003c,
-	0x00000062,
-} // Size: 44 bytes
+	0x00000062, 0x00000076, 0x000000d7,
+} // Size: 52 bytes
 
-const enData string = "" + // Size: 98 bytes
+const enData string = "" + // Size: 215 bytes
 	"\x02New purchase\x02Purchase of %[1]v from %[2]v.\x02Payment received" +
-	"\x02Payment received of %[1]v from %[2]v."
+	"\x02Payment received of %[1]v from %[2]v.\x02New payment request\x02%[2]" +
+	"v requests a payment of %[1]v. You should accept or reject this payment " +
+	"as soon as possible."
 
-var esIndex = []uint32{ // 5 elements
+var esIndex = []uint32{ // 7 elements
 	0x00000000, 0x0000000d, 0x00000026, 0x00000034,
-	0x00000051,
-} // Size: 44 bytes
+	0x00000051, 0x00000066, 0x000000d3,
+} // Size: 52 bytes
 
-const esData string = "" + // Size: 81 bytes
+const esData string = "" + // Size: 211 bytes
 	"\x02Nueva compra\x02Compra de %[1]v a %[2]v.\x02Pago recibido\x02Has rec" +
-	"ibido %[1]v de %[2]v."
+	"ibido %[1]v de %[2]v.\x02Nuevo pago pendiente\x02Tienes un nuevo pago de" +
+	" %[1]v para %[2]v. Por favor acepta o rechaza la transacción lo más pron" +
+	"to posible."
 
-	// Total table size 389 bytes (0KiB); checksum: 2637B30D
+	// Total table size 786 bytes (0KiB); checksum: 1AEC5E69
