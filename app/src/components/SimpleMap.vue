47ba18816b6a480269afb7eb6510208f75ad1115
Esteve Badia <badia.orive@gmail.com>
2024-02-20 15:06:19 +0100

    Dont wheel zoom on non-interactive maps

--- app/src/components/SimpleMap.vue
+++ app/src/components/SimpleMap.vue
@@ -1,6 +1,6 @@
 <template>
   <l-map
-    :options="{ zoomControl: false, dragging: interactive, attributionControl: false }"
+    :options="{ zoomControl: false, dragging: interactive, attributionControl: false, scrollWheelZoom: interactive }"
     style="height: 200px; width: 100% ; margin: 0; z-index:0;"
     :zoom="zoom"
     :center="centerLatLng"
