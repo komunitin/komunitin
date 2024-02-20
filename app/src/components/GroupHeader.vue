1bdc71c2b669cac35c2dc30061a89eacbf59c7a7
Esteve Badia <badia.orive@gmail.com>
2024-02-20 15:06:37 +0100

    Factor GroupHeader

---
+++ app/src/components/GroupHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-item
+    class="text-onsurface-m"
+  >
+    <q-item-section avatar>
+      <avatar
+        :img-src="group.attributes.image"
+        :text="group.attributes.code"
+      />
+    </q-item-section>
+    <q-item-section>
+      <div class="text-subtitle2">
+        {{ group.attributes.name }}
+      </div>
+      <q-item-label caption>
+        {{
+          group.attributes.code
+        }}
+      </q-item-label>
+    </q-item-section>
+    <q-item-section side>
+      <!-- No content by default on the side slot -->
+      <slot name="side" />
+    </q-item-section>
+  </q-item>
+</template>
+<script setup lang="ts">
+import { Group } from 'src/store/model';
+import Avatar from './Avatar.vue'
+
+const props = defineProps<{
+  group: Group
+}>()
+</script>
