da6f261985299176cbb2883ff5a814995f05f457
Esteve Badia <badia.orive@gmail.com>
2024-03-07 15:04:43 +0100

    Signup test

--- app/src/components/InactiveBanner.vue
+++ app/src/components/InactiveBanner.vue
@@ -18,15 +18,18 @@
   </q-banner>
 </template>
 <script setup lang="ts">
-import {computed} from "vue";
-import {useStore} from "vuex";
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import { useStore } from "vuex";
 
 const store = useStore()
+const route = useRoute()
 
 const dismissed = computed(() => store.state.ui.inactiveBannerDismissed)
 const dismissInactive = () => store.commit("inactiveBannerDismissed", true)
+const isSignupMemberPage = computed(() => route.name === "SignupMember")
 
-const show = computed(() => !dismissed.value && store.getters.isLoggedIn && !store.getters.isActive)
+const show = computed(() => !dismissed.value && store.getters.isLoggedIn && !store.getters.isActive && !isSignupMemberPage.value)
 
 defineExpose({show})
 
