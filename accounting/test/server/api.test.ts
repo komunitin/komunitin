82ee7be478677780ad62e7fe039016bb32bfd402
Esteve Badia <badia.orive@gmail.com>
2024-05-18 13:24:51 +0200

    Fix server test

--- accounting/test/server/api.test.ts
+++ accounting/test/server/api.test.ts
@@ -37,10 +37,10 @@ describe('The Komunitin accounting API', async () => {
     assert(isUuid(response.body.data.id), "The currency id is not a valid UUID")
     assert.equal(response.body.data.type, 'currencies')
     assert.equal(response.body.data.attributes.code, 'TEST')
-    assert.equal(response.body.data.attributes.name, 'Test currency')
+    assert.equal(response.body.data.attributes.name, 'Testy')
     assert.equal(response.body.data.attributes.rate.n, 1)
     assert.equal(response.body.data.attributes.rate.d, 10)
-    assert.equal(response.body.data.attributes.defaultInitialCredit, '1000')
+    assert.equal(response.body.data.attributes.defaultCreditLimit, 1000)
   })
 
   it('GET /currencies', async () => {
