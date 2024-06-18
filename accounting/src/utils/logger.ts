53f51dffbe2129a149e9f383b1eba3f1ffdc86bc
Esteve Badia <badia.orive@gmail.com>
2024-06-18 15:35:01 +0200

    Dont log all headers

--- accounting/src/utils/logger.ts
+++ accounting/src/utils/logger.ts
@@ -5,6 +5,29 @@ export const logger = pino({
   name: "accounting",
 })
 
+const filterObjectKeys = (obj: Record<string, any> | undefined, keys: string[]) => {
+  return obj === undefined ? undefined : Object.fromEntries(
+    Object.entries(obj).filter(([key]) => keys.includes(key))
+  )
+}
+
 export const httpLogger = pinoHttp({
-  logger: pino({name: "http"})
+  logger: pino({name: "http"}),
+  serializers: {
+    // Dont log all request headers, and specially don't log the credentials.
+    req: pino.stdSerializers.wrapRequestSerializer((req) => {
+      const {headers, ...rest} = req
+      return {
+        ...rest,
+        headers: filterObjectKeys(req.raw.headers, ["host", "user-agent", "x-forwarded-for", "referer"])
+      }
+    }),
+    // Dont log all response headers.
+    res: pino.stdSerializers.wrapResponseSerializer((res: any) => {
+      return {
+        statusCode: res.raw.statusCode,
+        headers: filterObjectKeys(res.raw.headers, ["content-type", "content-length"])
+      }
+    })
+  }
 })
