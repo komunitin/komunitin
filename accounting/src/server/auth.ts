70e824ea0c2f070ce662bb4d661933731ff6259d
Esteve Badia <badia.orive@gmail.com>
2024-07-03 12:21:41 +0200

    Refresh JWKS after invalid token error / 5 min

--- accounting/src/server/auth.ts
+++ accounting/src/server/auth.ts
@@ -1,27 +1,32 @@
 
-import { auth as authJwt, scopeIncludesAny } from "express-oauth2-jwt-bearer"
+import { InvalidTokenError, auth as authJwt, scopeIncludesAny } from "express-oauth2-jwt-bearer"
 import { config } from "../config"
 import { NextFunction,Request, Response } from "express"
 import { fixUrl } from "src/utils/net"
+import { logger } from "src/utils/logger"
 
 export enum Scope {
   Accounting = "komunitin_accounting",
   AccountingReadAll = "komunitin_accounting_read_all",
 }
 
-const jwt = authJwt({
-  issuer: config.AUTH_JWT_ISSUER,
-  audience: config.AUTH_JWT_AUDIENCE,
-  jwksUri: fixUrl(config.AUTH_JWKS_URL),
-  validators: {
-    // IntegralCES creates JWTs with a null sub claim for the tokens
-    // requested by the notifications service. The default validator
-    // in express-oauth2-jwt-bearer does not allow null values for 
-    // the sub claim.
-    sub: (sub) => typeof sub === "string" || sub === null,
-  }
-})
+const buildJwt = () => {
+  return authJwt({
+    issuer: config.AUTH_JWT_ISSUER,
+    audience: config.AUTH_JWT_AUDIENCE,
+    jwksUri: fixUrl(config.AUTH_JWKS_URL),
+    validators: {
+      // IntegralCES creates JWTs with a null sub claim for the tokens
+      // requested by the notifications service. The default validator
+      // in express-oauth2-jwt-bearer does not allow null values for 
+      // the sub claim.
+      sub: (sub) => typeof sub === "string" || sub === null,
+    },
+  })
+}
 
+let jwt = buildJwt()
+let lastInvalidTokenRetry = 0
 /**
  * Require a valid JWT token in the request. If the scopes parameter is provided, require also
  * that the JWT includes at least one of the scopes in the parameter.
@@ -30,16 +35,30 @@ const jwt = authJwt({
  */
 export const auth = (scopes?: Scope|Scope[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    jwt(req, res, (err) => {
-      if (err) {
-        next(err)
-      } else if (scopes && scopes.length){
-         scopeIncludesAny(scopes)(req, res, next)
-      } else {
-        next()
-      }
-    })
+    handleAuthRequest(scopes, req, res, next)
   }
+}
+
+const handleAuthRequest = (scopes: Scope|Scope[]|undefined, req: Request, res: Response, next: NextFunction) => {
+  jwt(req, res, (err) => {
+    if (err) {
+      if (err instanceof InvalidTokenError && lastInvalidTokenRetry < Date.now() - 1000 * 60 * 5) {
+        // In this case it could be possible that the error is "signature verification failed" because the token
+        // is signed with a newly rotated key that is still not used because the jwks cache is not updated.
+        // Note that in order to prevent abuse, we only retry once every 5 minutes.
+        lastInvalidTokenRetry = Date.now()
+        jwt = buildJwt()
+        logger.warn("Invalid token error. Refreshing JWKS.")
+        handleAuthRequest(scopes, req, res, next)
+      } else {
+        next(err)
+      }
+    } else if (scopes && scopes.length){
+       scopeIncludesAny(scopes)(req, res, next)
+    } else {
+      next()
+    }
+  })
 }
 
 /**
