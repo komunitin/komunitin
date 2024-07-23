df3603d7fb5210125945173041aa0af1f8cde1c8
Esteve Badia <badia.orive@gmail.com>
2024-07-23 18:22:43 +0200

    Update Needs.spec.ts

--- app/test/jest/__tests__/Needs.spec.ts
+++ app/test/jest/__tests__/Needs.spec.ts
@@ -82,10 +82,12 @@ describe("Needs", () => {
     expect(text).toContain("Computers");
 
     await wrapper.get(".q-btn--fab").trigger("click");
+    await flushPromises();
     await wrapper.vm.$wait();
 
     expect(wrapper.vm.$route.path).toBe("/groups/GRP0/members/EmilianoLemke57");
     expect(wrapper.vm.$route.hash).toBe("#needs");
+    await flushPromises();
     await wrapper.vm.$wait();
 
     const text2 = wrapper.text();
