d5639269c75e64b18d0400967d1c361551bf055f
Esteve Badia <badia.orive@gmail.com>
2024-12-20 15:51:05 +0100

    Remove Location Banner (don't ask for location)

--- app/src/layouts/Banner.vue
+++ app/src/layouts/Banner.vue
@@ -6,20 +6,14 @@
     v-if="!(inactiveBanner?.show)"
     ref="notificationsBanner"
   />
-  <location-banner
-    v-if="!(inactiveBanner?.show || notificationsBanner?.show)"
-    ref="locationBanner"
-  />
 </template>
 <script setup lang="ts">
 import { ref } from "vue"
 import NotificationsBanner from "../components/NotificationsBanner.vue"
-import LocationBanner from "../components/LocationBanner.vue"
 import InactiveBanner from "../components/InactiveBanner.vue"
 
 const inactiveBanner = ref<InstanceType<typeof InactiveBanner>>()
 const notificationsBanner = ref<InstanceType<typeof NotificationsBanner>>()
-const locationBanner = ref<InstanceType<typeof LocationBanner>>()
 
 </script>
 <style lang="scss" scoped>
