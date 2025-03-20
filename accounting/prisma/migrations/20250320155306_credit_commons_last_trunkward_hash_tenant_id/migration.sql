4ebb41c2b5b10eb7ecdf398e7d254951a3893bb5
Michiel de Jong <michiel@pondersource.com>
2025-03-20 16:57:03 +0100

    Remove unparseable default values

--- accounting/prisma/migrations/20250320155306_credit_commons_last_trunkward_hash_tenant_id/migration.sql
+++ accounting/prisma/migrations/20250320155306_credit_commons_last_trunkward_hash_tenant_id/migration.sql
@@ -5,39 +5,6 @@
 
 */
 -- AlterTable
-ALTER TABLE "Account" ALTER COLUMN "tenantId" SET DEFAULT (current_setting('app.current_tenant_id'))::text;
-
--- AlterTable
-ALTER TABLE "AccountTag" ALTER COLUMN "tenantId" SET DEFAULT (current_setting('app.current_tenant_id'))::text;
-
--- AlterTable
-ALTER TABLE "AccountUser" ALTER COLUMN "tenantId" SET DEFAULT (current_setting('app.current_tenant_id'))::text;
-
--- AlterTable
 ALTER TABLE "CreditCommonsTrunkwardNode" DROP CONSTRAINT "CreditCommonsTrunkwardNode_pkey",
-ADD COLUMN     "tenantId" VARCHAR(31) NOT NULL DEFAULT (current_setting('app.current_tenant_id'))::text,
+ADD COLUMN     "tenantId" VARCHAR(31),
 ADD CONSTRAINT "CreditCommonsTrunkwardNode_pkey" PRIMARY KEY ("tenantId", "ccNodeName");
-
--- AlterTable
-ALTER TABLE "Currency" ALTER COLUMN "tenantId" SET DEFAULT (current_setting('app.current_tenant_id'))::text;
-
--- AlterTable
-ALTER TABLE "EncryptedSecret" ALTER COLUMN "tenantId" SET DEFAULT (current_setting('app.current_tenant_id'))::text;
-
--- AlterTable
-ALTER TABLE "ExternalResource" ALTER COLUMN "tenantId" SET DEFAULT (current_setting('app.current_tenant_id'))::text;
-
--- AlterTable
-ALTER TABLE "ExternalTransfer" ALTER COLUMN "tenantId" SET DEFAULT (current_setting('app.current_tenant_id'))::text;
-
--- AlterTable
-ALTER TABLE "Transfer" ALTER COLUMN "tenantId" SET DEFAULT (current_setting('app.current_tenant_id'))::text;
-
--- AlterTable
-ALTER TABLE "Trustline" ALTER COLUMN "tenantId" SET DEFAULT (current_setting('app.current_tenant_id'))::text;
-
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "tenantId" SET DEFAULT (current_setting('app.current_tenant_id'))::text;
-
--- AlterTable
-ALTER TABLE "Value" ALTER COLUMN "tenantId" SET DEFAULT (current_setting('app.current_tenant_id'))::text;
