94144ea34d3eca1abceb8dd8afbc352f5b99375d
Michiel de Jong <michiel@pondersource.com>
2025-03-25 16:12:52 +0100

    RLS for CreditCommonsNode table

--- accounting/prisma/migrations/20250325151032_credit_commons_node/migration.sql
+++ accounting/prisma/migrations/20250325151032_credit_commons_node/migration.sql
@@ -39,3 +39,15 @@ CREATE TABLE "CreditCommonsNode" (
 
     CONSTRAINT "CreditCommonsNode_pkey" PRIMARY KEY ("tenantId","ccNodeName")
 );
+
+-- Enable Row Level Security
+ALTER TABLE "CreditCommonsNode" ENABLE ROW LEVEL SECURITY;
+
+-- Force Row Level Security for table owners
+ALTER TABLE "CreditCommonsNode" FORCE ROW LEVEL SECURITY;
+
+-- Create row security policies
+CREATE POLICY tenant_isolation_policy ON "CreditCommonsNode" USING ("tenantId" = current_setting('app.current_tenant_id', TRUE)::text);
+
+-- Create policies to bypass RLS
+CREATE POLICY bypass_rls_policy ON "CreditCommonsNode" USING (current_setting('app.bypass_rls', TRUE)::text = 'on');
