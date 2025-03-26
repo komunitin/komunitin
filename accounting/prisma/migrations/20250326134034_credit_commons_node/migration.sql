0a0e34bd9eb59423347ce8cf7c6c854a385c3942
Michiel de Jong <michiel@pondersource.com>
2025-03-26 14:42:40 +0100

    Add vostro account

--- accounting/prisma/migrations/20250326134034_credit_commons_node/migration.sql
+++ accounting/prisma/migrations/20250326134034_credit_commons_node/migration.sql
@@ -3,9 +3,13 @@ CREATE TABLE "CreditCommonsNode" (
     "tenantId" VARCHAR(31) NOT NULL DEFAULT (current_setting('app.current_tenant_id'))::text,
     "ccNodeName" VARCHAR(255) NOT NULL,
     "lastHash" VARCHAR(255) NOT NULL,
+    "vostroId" TEXT NOT NULL,
 
     CONSTRAINT "CreditCommonsNode_pkey" PRIMARY KEY ("tenantId","ccNodeName")
 );
+
+-- AddForeignKey
+ALTER TABLE "CreditCommonsNode" ADD CONSTRAINT "CreditCommonsNode_vostroId_tenantId_fkey" FOREIGN KEY ("vostroId", "tenantId") REFERENCES "User"("id", "tenantId") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- Enable Row Level Security
 ALTER TABLE "CreditCommonsNode" ENABLE ROW LEVEL SECURITY;
